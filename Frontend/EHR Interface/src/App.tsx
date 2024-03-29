733912e70c381bfcc9a010122ebe04a8430d2019
sudheer0071 <sudheer1614@gmail.com>
2024-03-29 14:35:21 +0530

    deploying frontend

--- Frontend/EHR Interface/src/App.tsx
+++ Frontend/EHR Interface/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, redirect, Navigate, useNavigate, Router, useNavigation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route,Link, redirect, Navigate, useNavigate, Router, useNavigation } from 'react-router-dom'
 import { Signin } from './pages/users-panel/Signin'
 import { Signup } from './pages/users-panel/Signup'
 // import { Dashboard } from './pages/Dashboard'
@@ -10,29 +10,29 @@ import { backendDown, navState } from './pages/atom'
 import { useRecoilState } from 'recoil'  
 import  Users  from './pages/users-panel/Users';
 import Doctor from './pages/doctors panel/doctor-panel';
+import { Button } from './components/Button';
+import { Rouute } from './Rouute';
 
 
 function App() {
-  const [logged, setLogged] = useRecoilState(navState)
-  
-  const [isloading, setIsloading] = useState(true)
-  const [isbackendDown,setIsbackDown] = useRecoilState(backendDown) 
-
+  const [logged, setLogged] = useRecoilState(navState);
+  const [isloading, setIsloading] = useState(true);
+  const [isbackendDown, setIsbackDown] = useRecoilState(backendDown); 
   return (
     <div>
-          {/* <PdfComp/> */} 
+      {/* <PdfComp/> */}
+      <div> 
+        
+      </div>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/backendDown' element={<BackendDown/>}></Route> */}
-          {/* <Route path='/' element={isbackendDown?<BackendDown/>:isloading?<Loader/>:<Navigate to='/signin' />}></Route>  */}
-          <Route path='/users//*' element={<Users />}></Route>
-          <Route path='doctors//*' element={<Doctor />}></Route>
-          
-          {/* <Route path='/dashboard' element={isbackendDown?<BackendDown/>:logged ? <Dashboard /> : <Navigate to='/signin' />}></Route> */}
+          <Route path='/' element={<Rouute/>} ></Route>
+          <Route path="/users//*" element={<Users />} />
+          <Route path="/doctors//*" element={<Doctor />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 } 
 
 export default App
