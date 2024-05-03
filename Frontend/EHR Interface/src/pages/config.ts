7fe5ca76641a687bcecf08458000cc0d44487580
sudheer0071 <sudheer1614@gmail.com>
2024-05-03 12:30:38 +0530

    removing fson files

--- Frontend/EHR Interface/src/pages/config.ts
+++ Frontend/EHR Interface/src/pages/config.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "http://localhost:5000" 
-export const BACKEND_URL = "https://ehr-undeployed.onrender.com"  
+export const BACKEND_URL = "http://localhost:5000" 
+// export const BACKEND_URL = "https://ehr-undeployed.onrender.com"  
 export const FLASK_URL = " http://127.0.0.1:5000" 
